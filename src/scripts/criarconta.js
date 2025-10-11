8ad7afc04af3f8d5630512e65efe70050555317b
Lucas-Gama360 <lucasrgabrieldev@gmail.com>
2025-10-11 17:13:36 -0300

    vetor ciar conta

---
+++ src/scripts/criarconta.js
@@ -0,0 +1,14 @@
+
+function armazenar (){
+    email = document.areaDeLogin.email.value
+    user = document.areaDeLogin.usuario.value
+    senha = document.areaDeLogin.senha.value
+
+    const dadosusers = {
+        email: email,
+        usuario: user,
+        senha: senha
+    }
+
+    const registros = [dadosusers]
+}
